60d52e6ab65e7426666bf614e6de519ec83bc461
Yanndeo <yan2sambou@gmail.com>
2019-06-14 12:52:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SendMeMail
+Node js app to send automatic email
+
+#### What you need to run this code
+1. Node (10.15.3)
+2. NPM (6.4.1)
+3. Express 4.17.1
---
+++ app.js
@@ -0,0 +1,73 @@
+import cron from "node-cron"
+import express from "express"
+import fs from "fs"
+import nodemailer from "nodemailer";
+
+
+const app = express()
+const PORT = 5000;
+const email = "platformweb7@gmail.com";
+const passwd = "3W.G0D.m@";
+const sendTo = "yan2sambou@gmail.com"
+
+//Create Mail transporter
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass: passwd
+  }
+});
+
+//Send email chaque vendredi
+cron.schedule("* * * * Friday", ()=>{
+
+    console.log("---------------------");
+
+    console.log("Running Cron Job2");
+
+    let mailOptions = {
+        from: email ,
+        to: sendTo,
+        subject: "CronJOb from node js and mailer",
+        text: `hi there , thi semail was automaticcally send bu node server`
+    };
+
+    transporter.sendMail(mailOptions, (err, info)=>{
+        if(err){
+            throw err;
+        }else{
+            console.log(info)
+            fs.write('success.log', info, error=>{
+                if (error) throw error;
+                console.log("echec to write to the success file ");
+            })
+        }
+    })
+
+
+})
+
+
+
+
+
+cron.schedule("* * * * * " , ()=>{
+    console.log(" ------------------- ")
+
+    console.log('Runing cron job');
+
+
+    fs.writeFile('error.log','test cronjob', err=> {
+
+        if(err) throw err;
+        console.log("Successfully write to the error file ");
+    });
+})
+
+
+
+
+app.listen(PORT , ()=>{
+    console.log("j'ecoute sur "+ PORT)
+})
